d0512f2a6721e9c822e2d71b4ebfe4d3bbde477f
Rogerio Mac <rogeriobr740@live.com>
2020-02-13 23:58:20 -0500

    No errors

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = ({ title, icon }) => {
             <li>Hello {user && user.name}</li>
             <li>
                 <a href='#!'>
-                    <i className='fas fa-sigh-out-alt'></i>
+                    <i className='fas fa-sign-out-alt'></i>
                     <span className='hide-sm'>Logout</span>
                 </a>
             </li>
@@ -26,13 +26,13 @@ const Navbar = ({ title, icon }) => {
     const guestLinks = (
         <Fragment>
             <li>
-                <Link to='/about'>About</Link>
-            </li>
-            <li>
                 <Link to='/register'>Register</Link>
             </li>
             <li>
                 <Link to='/login'>Login</Link>
+            </li>
+            <li>
+                <Link to='/about'>About</Link>
             </li>
         </Fragment>
     );
